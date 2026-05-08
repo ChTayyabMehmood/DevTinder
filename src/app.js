1d8c2de09ece96937a58960d552245ab260570f5
ChTayyabMehmood <chtayyabmehmod@gmail.com>
2026-05-08 16:10:41 +0500

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express = require("express")
 const app =express()
-app.listen(3000 , ()=>{
-    console.log("server is listen sucessfull")
-});
+
 app.use('/home',(req,res)=>{
     res.send("hello from the okay");
 
 })
+
+app.get('/user',(req,res)=>{
+    res.send({"firstName":"Tayyab","lastName":"Jutt"})
+})
+
+app.post('/user',(req,res)=>{
+    console.log("saving data to db")
+      res.send("Data saved successfully")
+})
+
+app.delete('/user',(req,res)=>{
+    res.send("Data is deleted")
+})
+
+
+app.use('/user',(req,res)=>{
+    res.send("this is message from use")
+})
+
+
+app.listen(3000 , ()=>{
+    console.log("server is listen sucessfull")
+});
+
